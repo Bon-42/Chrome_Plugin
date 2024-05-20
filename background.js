1ae027b32a8bc203f739c58f2db53c5cd8658b07
bon-42 <teslaeinstein@Yantra.local>
2024-05-20 09:14:13 -0600

    updated links

--- background.js
+++ background.js
@@ -2,7 +2,8 @@ chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
     // Check if the request is for a main frame (page)
     if (details.type === "main_frame") {
-      const allowedDomains = ['youtube.com', 'leetcode.com', 'github.com', 'chat.openai.com'];
+      const allowedDomains = ['https://www.youtube.com/watch?v=P48QELwruQs', 'https://www.youtube.com/watch?v=gCWaRhNUvfc', 'https://www.youtube.com/watch?v=FxJ3zPUU6Y4',
+      'https://www.youtube.com/watch?v=mg7netw1JuM', 'leetcode.com', 'github.com', 'chat.openai.com', 'https://seanprashad.com/'] ;
       const url = new URL(details.url);
       const isAllowed = allowedDomains.some(domain => url.hostname.includes(domain));
       return { cancel: !isAllowed };
